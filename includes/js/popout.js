60e8afd8ad14ed793d7b02a9de21d94d3adca21b
maxwelllucasr <55898589+maxwelllucasr@users.noreply.github.com>
2020-01-24 11:04:42 -0500

    some stuff

--- includes/js/popout.js
+++ includes/js/popout.js
@@ -1,20 +1,20 @@
-jQuery(document).ready(function($){
+// jQuery(document).ready(function($){
 
 
-    $('.hamburger-menu').click(function(e){
-        // $('.popout-menu').toggleClass('hide');
-        $('.popout-menu').toggleClass('popout-is-open');
-        $('.hamburger-menu').toggleClass('fa-bars');
-        $('.hamburger-menu').toggleClass('fa-times');
+//     $('.hamburger-menu').click(function(e){
+//         // $('.popout-menu').toggleClass('hide');
+//         $('.popout-menu').toggleClass('popout-is-open');
+//         $('.hamburger-menu').toggleClass('fa-bars');
+//         $('.hamburger-menu').toggleClass('fa-times');
 
 
 
-    })
+//     })
 
-    var negative = "-";
-    var popupHeight = $('.popout-menu').height();
+//     var negative = "-";
+//     var popupHeight = $('.popout-menu').height();
     
-    $('.header-layout').css('margin-bottom', negative.concat(popupHeight)+ "px")
+//     $('.header-layout').css('margin-bottom', negative.concat(popupHeight)+ "px")
 
 
-});
+// });
