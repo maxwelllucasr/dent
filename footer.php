01a93d377b16c8042fce707a8d33ed64477a6307
maxwelllucasr <55898589+maxwelllucasr@users.noreply.github.com>
2020-03-10 16:43:31 -0400

    footer

--- footer.php
+++ footer.php
@@ -5,23 +5,35 @@
 /* ------------------------------------------------------------------------- */	
 
 // todo replace all text with variables for multilingual
-$footer_background = get_field('footer_background', 'options');
+// $footer_background = get_field('footer_background', 'options');
+$facebook = get_field('facebook', 'options');
+$instagram = get_field('instagram', 'options');
+$logo = get_field('header_logo','options');
+$donate = get_field('donate','options');
 ?>
 
 <footer>    
-    <div class="columns">
-        <div class="column is-3 footer-left"><a href="/"><span class="footer-rugged">Test</span> </a></div>
-        <div class="column is-9 footer-right">
-            <?php wp_nav_menu( array( 
-                'container'=> false,
-                'theme_location' => 'footer',
-                'depth'             => 2,
-                'menu_class' => 'footer-menu',
-                'menu_id' => 'footer-menu',
-								
-            )); ?>
-        </div>
+    <div class="container">
+        <div class="columns">
+            <div class="column is-6">
+                <?php wp_nav_menu( array( 
+                                'container'=> false,
+                                'theme_location' => 'footer',
+                                'depth'             => 2,
+                                'menu_class' => 'footer-menu',
+                ));
+                ?>
 
+                <div class="social-icons">
+                    <a href="<?=$facebook?>"><i class="fab fa-facebook-square"></i></a>
+                    <a href="<?=$instagram?>"><i class="fab fa-instagram-square"></i></a>
+                </div>
+            </div>
+            <div class="column is-6">   
+                <a class="footer-donate" href="<?=$donate?>"><span>Donate</span></a>
+                <a class="footer-logo" href="/"><img src="<?=$logo['url']?>"></a>
+            </div>
+        </div>
     </div>
 </footer>
 </div> <!-- This is the wrapper DO NOT DELETE. -->
