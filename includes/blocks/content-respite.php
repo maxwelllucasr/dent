6b7d2d6b6942f5baebe5e8a1ac40dd060d1f30b9
maxwelllucasr <55898589+maxwelllucasr@users.noreply.github.com>
2020-03-10 17:09:35 -0400

    fixes

--- includes/blocks/content-respite.php
+++ includes/blocks/content-respite.php
@@ -14,6 +14,7 @@ $background = get_field('respite_background');
 
 <section class="respite-container" style="background-image: url(<?=$background['url']?>)">
 <div class="respite-inside"></div>
+<div class="container">
 
 
     <div class="columns is-centered">
@@ -21,12 +22,12 @@ $background = get_field('respite_background');
     </div>
 
     <div class="columns is-centered">
-        <div class="column is-6"><p class="respite-overlay"><?=$copy?></p></div>
+        <div class="column is-10"><p class="respite-overlay"><?=$copy?></p></div>
     </div>
 
     <div class="columns is-centered">
         <div class="column is-6"><a href="<?=$button['url']?>" class="respite-button respite-overlay"><span><?=$button['title']?></span></a></div>
     </div>
 
-
+</div>
 </section>
