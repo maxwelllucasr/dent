9c4ea663896f80b32c135a97d7f9c7c1a402edfd
maxwelllucasr <55898589+maxwelllucasr@users.noreply.github.com>
2020-03-18 13:25:57 -0400

    changes

--- footer.php
+++ footer.php
@@ -29,8 +29,24 @@ $donate = get_field('donate','options');
                     <a href="<?=$instagram?>"><i class="fab fa-instagram-square"></i></a>
                 </div>
             </div>
-            <div class="column is-6">   
-                <a class="footer-donate" href="<?=$donate?>"><span>Donate</span></a>
+            <div class="column is-6">  
+
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                <input type="hidden" name="cmd" value="_donations" />
+                <input type="hidden" name="business" value="maxwelllucasr@gmail.com" />
+                <input type="hidden" name="currency_code" value="USD" />
+                <input type="image" class="footer-donate-img" src="https://dent.jacksoncyberspace.com/wp-content/uploads/2020/03/Untitled.png" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+                <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+            </form>
+
+
+
+                <!-- <a class="footer-donate" href="<?=$donate?>"><span>Donate</span></a> -->
+
+
+
+
+
                 <a class="footer-logo" href="/"><img src="<?=$logo['url']?>"></a>
             </div>
         </div>
